5beb661fc3ad320a199e63ba65a0ff6eee4f83d6
AbrahimMahmud <abrahimmclutch21@gmail.com>
2021-07-10 23:59:02 -0400

    This is before adding google login.

--- app/src/main/java/com/example/attenda_attempt3/LoginActivity.java
+++ app/src/main/java/com/example/attenda_attempt3/LoginActivity.java
@@ -1,16 +1,14 @@
 package com.example.attenda_attempt3;
 
-    import androidx.appcompat.app.AppCompatActivity;
-    import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity{
+import android.os.Bundle;
 
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-
-            }
-
+        setContentView(R.layout.activity_login);
     }
+}
--- app/src/main/java/com/example/attenda_attempt3/SplashActivity.java
+++ app/src/main/java/com/example/attenda_attempt3/SplashActivity.java
@@ -13,20 +13,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        //after the splashscreen goes away, this prevents the user from returning back to the splashscreen if the user presses the back button
-        Runnable runnable=new Runnable() {
+        Runnable runnable = new Runnable() {
             @Override
-            public void run(){
-                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
                 finish();
             }
         };
-        //plays the splashscreen for 5000ms (2.5 seconds)
-        Handler handler=new Handler();
+        Handler handler = new Handler();
         handler.postDelayed(runnable, 2500);
     }
 }
